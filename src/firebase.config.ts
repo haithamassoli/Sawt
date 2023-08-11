5ef46f1a4a5ecae5dbb80a6cc7feb6d386633ed3
haithamassoli <haitham.b.assoli@gmail.com>
2023-08-11 15:10:24 +0300

    added firebase

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,30 +1,28 @@
-// import Constants from "expo-constants";
-// import { getAuth } from "firebase/auth";
-// import { initializeApp } from "firebase/app";
-// import { getStorage } from "firebase/storage";
-// import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
-// const {
-//   apiKey,
-//   authDomain,
-//   projectId,
-//   storageBucket,
-//   messagingSenderId,
-//   appId,
-//   measurementId,
-// } = Constants?.expoConfig?.extra?.firebase;
+const {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} = Constants?.expoConfig?.extra?.firebase;
 
-// const firebaseConfig = {
-//   apiKey: apiKey,
-//   authDomain: authDomain,
-//   projectId: projectId,
-//   storageBucket: storageBucket,
-//   messagingSenderId: messagingSenderId,
-//   appId: appId,
-//   measurementId: measurementId,
-// };
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+};
 
-// const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app);
-// export const storage = getStorage(app);
-// export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
